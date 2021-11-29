eba85d9b423229ffaaf540dd38eb0059d5af1551
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-29 21:52:12 +0530

    Added:Welcome Message

---
+++ src/com/blz/lineComparison/Line_Comparison.java
@@ -0,0 +1,6 @@
+package com.blz.lineComparison;
+public class Line_Comparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+}
